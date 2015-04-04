d65a0b1f9b62f2c03c1c5b46d621227ffda48b69
Soli <soli@cbug.org>
2015-04-04 23:59:48 +0800

    launch!

---
+++ varname.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
+)
+
+func main() {
+	m := martini.Classic()
+	m.Use(render.Renderer(render.Options{
+		Directory: "app/views/simple",
+		Extensions: []string{".tmpl", ".html"},
+	}))
+
+	m.Get("/", func(r render.Render) {
+		r.HTML(200, "index", "Variable Naming Service")
+	})
+
+	m.Get("/:name", func(r render.Render, params martini.Params) {
+		r.JSON(200, map[string]interface{}{"hello": params["name"]})
+	})
+
+	m.Run()
+}
