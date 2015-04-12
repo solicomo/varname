1d70040bd80ae26284707b3237f8bd8013608bb1
Soli <soli@cbug.org>
2015-04-13 00:04:57 +0800

    search page;

--- varname.go
+++ varname.go
@@ -27,20 +27,19 @@ func main() {
 		data["SearchString"] = qs
 		data["SearchRelates"] = []string {qs, qs, qs, qs}
 		data["SearchList"] = []SearchRecord {
-			{qs, "/search?q=" + qs, qs + qs + qs},
-			{qs, "/search?q=" + qs, qs + qs + qs},
-			{qs, "/search?q=" + qs, qs + qs + qs},
-			{qs, "/search?q=" + qs, qs + qs + qs},
-			{qs, "/search?q=" + qs, qs + qs + qs},
-			{qs, "/search?q=" + qs, qs + qs + qs},
-			{qs, "/search?q=" + qs, qs + qs + qs},
+			{"Title: " + qs, qs, "Caption: " + qs + qs + qs},
+			{"Title: " + qs, qs, "Caption: " + qs + qs + qs},
+			{"Title: " + qs, qs, "Caption: " + qs + qs + qs},
+			{"Title: " + qs, qs, "Caption: " + qs + qs + qs},
+			{"Title: " + qs, qs, "Caption: " + qs + qs + qs},
+			{"Title: " + qs, qs, "Caption: " + qs + qs + qs},
 		}
 
 		r.HTML(200, "search", data)
 	})
 
-	m.Get("/:name", func(r render.Render, params martini.Params) {
-		r.JSON(200, map[string]interface{}{"hello": params["name"]})
+	m.Get("/e/:entry", func(r render.Render, params martini.Params) {
+		r.HTML(200, "entry", map[string]interface{}{"entry": params["entry"]})
 	})
 
 	m.RunOnAddr(":8080")
