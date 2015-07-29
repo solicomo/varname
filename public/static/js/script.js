10393212521e9f922351fb526876402317609376
Soli <soli@cbug.org>
2015-07-30 01:59:56 +0800

    toggle comments' display;

--- public/static/js/script.js
+++ public/static/js/script.js
@@ -12,6 +12,32 @@ function showTabFor(menuBtn) {
 	isClearTabs = false;
 	return false;
 }
+
+function toggle_comments(id) {
+	var blk = document.getElementById(id);
+	
+	if (blk === null) {
+		return;
+	}
+
+	if (blk.style.display != 'none') {
+		blk.setAttribute('data-display', blk.style.display);
+		blk.style.display = 'none';
+	} else {
+		var display = blk.getAttribute('data-display');
+
+		if (display === null || display.length < 1) {
+			display = 'block';
+		}
+		blk.style.display = display;
+	}
+
+	var btn = document.getElementById('entry-comments-tgicon');
+	if (btn !== null) {
+		btn.classList.toggle('fa-rotate-180');
+	}
+}
+
 document.onclick = function() {
 	if (isClearTabs) {
 		clearTabs();
